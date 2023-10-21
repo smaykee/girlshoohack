9f9329bcffe919458dc354e0a3821eb149314a97
suwanl0620 <suwanl0620@gmail.com>
2023-10-21 14:14:25 -0400

    Created Bathroom class

---
+++ lib/bathroom.dart
@@ -0,0 +1,8 @@
+class Bathroom {
+  final String name;
+  final double latitude;
+  final double longitude;
+  final String address;
+
+  Bathroom({this.name = '', this.latitude = 0.0, this.longitude = 0.0, this.address = ''});
+}
