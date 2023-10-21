90fa2b2dc1a31afca7495034b1b13d5b0f5b4a7c
suwanl0620 <suwanl0620@gmail.com>
2023-10-21 16:51:27 -0400

    Created sample data to bathroom.dart

--- lib/bathroom.dart
+++ lib/bathroom.dart
@@ -2,7 +2,13 @@ class Bathroom {
   final String name;
   final double latitude;
   final double longitude;
-  final String address;
 
-  Bathroom({this.name = '', this.latitude = 0.0, this.longitude = 0.0, this.address = ''});
+  Bathroom({this.name = '', this.latitude = 0.0, this.longitude = 0.0});
 }
+
+final List<Bathroom> sampleBathrooms = [
+  Bathroom(
+      name: 'Rotunda Bathroom 1', latitude: 38.035537, longitude: -78.503521),
+  Bathroom(
+      name: 'Rice Hall Bathroom 1', latitude: 38.031611, longitude: -78.510728)
+];
